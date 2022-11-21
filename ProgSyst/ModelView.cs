a3178893042290e6d7dcea4b398b94d5dcd3d620
BaditSad <bdumortier0@gmail.com>
2022-11-21 15:30:06 +0100

    modif

---
+++ ProgSyst/ModelView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Système
+{
+    class ModelViewSave
+    {
+        Model model;
+        View view;
+
+    }
+    class ModelViewShow
+    {
+        Model model;
+        View view;
+    }
+}
---
+++ ProgSyst/View.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Système
+{
+    class View
+    {
+        public View()
+        {
+            //Constructor
+        }
+        public void CreateSave()
+        {
+            Console.WriteLine(created_save);
+        }
+        public void ShowSave()
+        {
+            Console.WriteLine(show_save);
+        }
+    }
+}
